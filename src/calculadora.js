882523c911cf96461e24072bdcc45dd1677da5e0
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-05-10 06:49:45 -0300

    First

---
+++ src/calculadora.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './calculadora.css';
+import { Card, Jumbotron } from 'react-bootstrap';
+
+function Calculadora() {
+  return (
+    <Jumbotron style={{ 
+      background:'transparent !important',
+      backgroundColor: '#007bff'
+    }} >
+
+    </Jumbotron>
+  );
+}
+
+export default Calculadora;
